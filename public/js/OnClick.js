c161ecbc2775a9215df01365d9f0087814600422
ubc-haptipedia <40783159+ubc-haptipedia@users.noreply.github.com>
2018-07-03 23:18:11 -0700

    Update OnClick.js

--- public/js/OnClick.js
+++ public/js/OnClick.js
@@ -9,7 +9,7 @@ var storeClickArrayCount = [];
 var flag = 0;
 var storeClick = 0;
 var tempDevice;
-$('.point81').on("click", function(e){
+$('.point2').on("click", function(e){
 	if(e.metaKey || e.ctrlKey){
 		
 		var clickData = [];
