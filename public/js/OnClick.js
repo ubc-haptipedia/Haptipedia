3681a578e257907ef2a466072b0e0da4a99955ad
ubc-haptipedia <40783159+ubc-haptipedia@users.noreply.github.com>
2018-07-03 23:00:43 -0700

    point2 to data.eq(1)

--- public/js/OnClick.js
+++ public/js/OnClick.js
@@ -9,7 +9,7 @@ var storeClickArrayCount = [];
 var flag = 0;
 var storeClick = 0;
 var tempDevice;
-$('.point2').on("click", function(e){
+$('.data').eq(1).on("click", function(e){
 	if(e.metaKey || e.ctrlKey){
 		
 		var clickData = [];
