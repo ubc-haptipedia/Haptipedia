65ae6da606ef4cc7d9491edfdb5e051290f5fdea
ubc-haptipedia <40783159+ubc-haptipedia@users.noreply.github.com>
2018-07-03 21:51:04 -0700

    Update app.js

--- app.js
+++ app.js
@@ -107,7 +107,7 @@ app.get('/readmetadata/:deviceid', function (req, res) {
 // app.get('/readfigures/:deviceid',(req,res) => var files = fs.readdirSync('/public/'+req.params.deviceid); res.send(files))
 
 
-app..listen(process.env.PORT || 5000);
+app.listen(process.env.PORT || 5000)
 
 
 
